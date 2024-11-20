148c79e85530127114d878620489ef5cdab7dffa
Andision <48558940+Andision@users.noreply.github.com>
2024-11-20 16:10:00 -0500

    bugfix: fix segments update

--- HashTable/SinglyLinkedListSegmented.cpp
+++ HashTable/SinglyLinkedListSegmented.cpp
@@ -1,15 +1,21 @@
 #include "SinglyLinkedListNode.cpp"
 #include <iostream>
 #include <vector>
+#define DEFAULT_SEGMENTS_SIZE 3
 using namespace std;
 
 template <typename K, typename V>
 class SinglyLinkedListSegmented {
 private:
     SinglyLinkedListNode<K, V> *head;
+    SinglyLinkedListNode<K, V> *tail;
+    int segmentSize;
+    int size = 0;
+    vector<SinglyLinkedListNode<K, V> *> segments;
 
 public:
-    SinglyLinkedListSegmented() : head(nullptr) {}
+    SinglyLinkedListSegmented() : head(nullptr), tail(nullptr), segmentSize(DEFAULT_SEGMENTS_SIZE) {}
+    SinglyLinkedListSegmented(int seg) : head(nullptr), tail(nullptr), segmentSize(seg) {}
 
     ~SinglyLinkedListSegmented() {
         SinglyLinkedListNode<K, V> *current = head;
@@ -29,10 +35,36 @@ public:
         cout << "nullptr" << endl;
     }
 
-    void insertAtHead(K key, V value) {
+    void insertAtTail(K key, V value) {
         SinglyLinkedListNode<K, V> *newNode = new SinglyLinkedListNode<K, V>(key, value);
-        newNode->next = head;
-        head = newNode;
+
+        if (!tail) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+
+        if (segments.size() < segmentSize) {
+            segments.push_back(newNode);
+        } else if (size % segmentSize == 1) {
+            int offset = 1;
+            for (auto it = segments.begin() + 1; it != segments.end(); ++it) {
+                SinglyLinkedListNode<K, V> *pivot = *it;
+                int t = offset;
+                while (t) {
+                    --t;
+                    pivot = pivot->next;
+                }
+                *it = pivot;
+                ++offset;
+            }
+
+            if (*(segments.end() - 1) == nullptr) {
+                segments.pop_back();
+            }
+        }
     }
 
     bool remove(K key) {
@@ -42,17 +74,39 @@ public:
         SinglyLinkedListNode<K, V> *previous = searchResult[0];
         SinglyLinkedListNode<K, V> *current = searchResult[1];
 
+        if (current == tail) {
+            tail = previous;
+        }
+
         if (!current) {
             return false;
         } else {
             SinglyLinkedListNode<K, V> *next = current->next;
-            previous->next = next;
+            if (previous) {
+                previous->next = next;
+            } else {
+                head = current->next;
+            }
             delete current;
+            size--;
+            segments.clear();
+            current = head;
+            int count = 0;
+            while (current) {
+                if (count == 0) {
+                    segments.push_back(current);
+                    count = (size - 1) / segmentSize;
+                } else {
+                    --count;
+                }
+
+                current = current->next;
+            }
             return true;
         }
     }
 
-    bool update(K key, V oldValue, V newValue) {
+    bool update(K key, V newValue) {
         SinglyLinkedListNode<K, V> *nodeToUpdate = search(key);
         if (nodeToUpdate) {
             nodeToUpdate->value = newValue;
@@ -80,5 +134,12 @@ public:
         }
 
         return {nullptr, nullptr};
+    }
+
+    void printSegments() {
+        cout << "printSegments:" << endl;
+        for (auto it = segments.begin(); it != segments.end(); ++it) {
+            cout << (*it)->key << endl;
+        }
     }
 };
---
+++ HashTable/SinglyLinkedListSegmented_test.cpp
@@ -0,0 +1,32 @@
+#include "SinglyLinkedListSegmented.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    SinglyLinkedListSegmented<int, char> list;
+
+    list.insertAtTail(1, '1');
+    list.insertAtTail(2, '2');
+    list.insertAtTail(3, '3');
+    list.insertAtTail(4, '4');
+    list.insertAtTail(5, '5');
+    list.insertAtTail(6, '6');
+    list.insertAtTail(7, '7');
+    list.printSegments();
+    list.insertAtTail(8, '8');
+    list.printSegments();
+    list.insertAtTail(9, '9');
+    list.printSegments();
+
+    cout << list.search(1)->value << endl;
+    cout << list.search(2)->value << endl;
+    cout << list.search(3)->value << endl;
+
+    list.remove(3);
+    list.printSegments();
+    list.remove(6);
+    list.printSegments();
+    list.remove(9);
+    list.printSegments();
+}
